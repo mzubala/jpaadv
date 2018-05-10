2331e552e950ce7bac18836ff8092c14f1dcecc9
Maciej Zubala <maciej.zubala@gmail.com>
2018-05-10 15:35:29 +0200

    integration tests

---
+++ src/test/java/pl/com/bottega/qma/integration/CreateDocumentTest.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.qma.integration;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CreateDocumentTest {
+
+
+
+}
