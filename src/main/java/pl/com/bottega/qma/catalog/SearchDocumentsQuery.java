4d40382500934a4364967f48d16411c1839e040d
Maciej Zubala <maciej.zubala@gmail.com>
2018-05-10 12:24:15 +0200

    api solution

--- src/main/java/pl/com/bottega/qma/catalog/SearchDocumentsQuery.java
+++ src/main/java/pl/com/bottega/qma/catalog/SearchDocumentsQuery.java
@@ -1,4 +1,13 @@
 package pl.com.bottega.qma.catalog;
 
-public class SearchDocumentsQuery {
+import java.util.Set;
+
+public class SearchDocumentsQuery  {
+
+  public String query;
+
+  public String status;
+
+  public Set<String> publishedForDepartments;
+
 }
--- src/main/java/pl/com/bottega/qma/confirmation/DocumentConfirmation.java
+++ src/main/java/pl/com/bottega/qma/confirmation/DocumentConfirmation.java
@@ -1,4 +1,16 @@
 package pl.com.bottega.qma.confirmation;
 
+import pl.com.bottega.qma.confirmation.commands.ConfirmDocumentCommand;
+import pl.com.bottega.qma.confirmation.commands.ConfirmDocumentOnBehalfCommand;
+
 public class DocumentConfirmation {
+
+  public void confirm(ConfirmDocumentCommand cmd) {
+
+  }
+
+  public void confirmFor(ConfirmDocumentOnBehalfCommand cmd) {
+
+  }
+
 }
